529442fa73ee70923158ab854fbceedce1d452bf
Rexwell Minnis <rexwell.minnis@citrix.com>
2015-05-24 08:13:22 -0400

    Cleanup functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,32 +3,33 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
-makeVector <- function(m = numeric()) {
-    my <- NULL
-    set <- function(y) {
-        x <<- y;
-        m <<- NULL;
+makeCacheMatrix <- function(my_x = matrix()) {
+    my_inv <- NULL
+    set <- function(x) {
+        my_x <<- x;
+        my_inv <<- NULL;
     }
-    get <- function() x;
-    setmean <- function(mean) my <<- mean;
-    getmean <- function() m;
-    list(set=set, get=get, 
-         setmean=setmean,
-         getmean=getmean)
+    get <- function() my_x;
+    setinv <- function(inv) my_inv <<- inv;
+    getinv <- function() my_inv;
+    list(set=set, 
+         get=get, 
+         setinv=setinv,
+         getinv=getinv)
 }
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 ## If the inverse has already been calculated (and the matrix has not changed), then the 
 ## cachesolve should retrieve the inverse from the cache.
 
-cachemean <- function(x, ...) {
-    m <- x$getinv()
-    if(!is.null(m)) {
-        message("getting cached data")
-           return(m)
+cacheSolve <- function(x, ...) {
+   my_inv <- x$getinv()
+    if(!is.null(my_inv)) {
+        message("Looking up data from cache")
+           my_inv
     }
     data <- x$get()
-    x <- mean(data, ...)
-    x$setmean(m)
-    m
+    my_inv <- solve(data, ...)
+    x$setinv(my_inv)
+    my_inv
 }
